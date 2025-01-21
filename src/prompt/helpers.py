d2762a33462334bdcedf6b3aecad00a283e01d96
Dylan-Kline <klinedylan46@gmail.com>
2025-01-21 15:01:07 -0500

    feat 027: Add additional considerations in the TODO and Implemented a function to prepare low level reflections parameters

--- src/prompt/helpers.py
+++ src/prompt/helpers.py
@@ -83,3 +83,77 @@ def prepare_latest_market_intelligence_params(state: Dict,
     })
 
     return res_params
+
+def prepare_low_level_reflection_params(state: Dict,
+                                        info: Dict,
+                                        params: Dict,
+                                        memory: MemoryInterface = None,
+                                        provider: EmbeddingProvider = None,
+                                        diverse_query: DiverseQuery = None
+                                        ):
+    
+    llr_params = deepcopy(params)
+
+    low_level_reflection_query = params["low_level_reflection_query"]
+    low_level_reflection_reasoning = params["low_level_reflection_reasoning"]
+
+    low_level_reflection_short_term_reasoning = low_level_reflection_reasoning["short_term_reasoning"]
+    low_level_reflection_medium_term_reasoning = low_level_reflection_reasoning["medium_term_reasoning"]
+    low_level_reflection_long_term_reasoning = low_level_reflection_reasoning["long_term_reasoning"]
+
+    # Grab latest low level reflection data and format as string
+    date = info["date"]
+    latest_llr = f"""Date: {date}\n
+                    Short-Term reasoning: {low_level_reflection_short_term_reasoning}.\n
+                    Medium-Term reasoning: {low_level_reflection_medium_term_reasoning}.\n
+                    Long-Term reasoning: {low_level_reflection_long_term_reasoning}.\n"""
+    
+    # Setup query params
+    query_params = {
+        "type": "low_level_reflection",
+        "symbol": params["asset_symbol"],
+        "query_text": low_level_reflection_query
+    }
+
+    # Query memory for similar low level reflections
+    query_result = diverse_query.query(params=query_params,
+                                       query_types=["plain"])
+    
+    past_llrs = list()
+    for query_type, values in query_result.items():
+        query_items = values["query_items"]
+
+        # Aggregate all past low level reflections for the current query type
+        query_type_reflections = list()
+        for reflection in query_items:
+
+            # Grab the reasonings for each time horizon (e.g. short, medium, and long term)
+            reasoning = reflection["reasoning"]
+            short_term_reasoning = reasoning["short_term"]
+            medium_term_reasoning = reasoning["medium_term"]
+            long_term_reasoning = reasoning["long_term"]
+
+            # Store all reflections in a corresponding string and append
+            reflection_text = f"""Date: {reflection['date']}\n
+                                  Short-Term reasoning: {short_term_reasoning}\n
+                                  Medium-Term reasoning: {medium_term_reasoning}\n
+                                  Long-Term reasoning: {long_term_reasoning}\n"""
+            query_type_reflections.append(reflection_text)
+        
+        if len(query_type_reflections) != 0:
+            type_text = "\n\n".join(query_type_reflections)
+            type_text = "The past low level reflection for " + query_type + " is:\n" + type_text
+            past_llrs.append(type_text)
+
+    if len(past_llrs) != 0:
+        past_low_level_reflection = "\n\n".join(past_llrs)
+    else:
+        past_low_level_reflection = "There is no past low level reflection as it is trading initialised."
+
+    llr_params.update({
+        "latest_low_level_reflection": latest_llr,
+        "past_low_level_reflection": past_low_level_reflection,
+    })
+
+    return llr_params 
+
